698aec3ca0994abb776dfe6c7ec918b90579ba81
yang <joey674901205@gmail.com>
2022-08-05 14:33:13 -0400

    add PUT, DELETE in post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
   console.log('===============');
   Post.findAll({
     attributes: ['id', 'title', 'content', 'created_at'],
+    order: [['created_at', 'DESC']],
     include: [
       {
         model: User,
@@ -60,6 +61,48 @@ router.post('/', (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      content: req.body.content
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        req.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
