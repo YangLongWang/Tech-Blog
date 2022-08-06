f82933d9901b087cf00781815fbf49c6f0b30703
yang <joey674901205@gmail.com>
2022-08-06 15:48:27 -0400

    add login and logout function, navigation of login and logout in main page, require express-session and connect-session-sequelize to store session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const routes = require('express').Router();
+const router = require('.');
 const { User, Post, Comment } = require('../../models');
 
 // GET /api/users
@@ -54,7 +55,15 @@ routes.post('/', (req, res) => {
     username: req.body.username,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -74,16 +83,34 @@ routes.post('/login', (req, res) => {
 
     // verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
-    res.json({ 
-      user: dbUserData,
-      message: 'You are now logged in!'  
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ 
+        user: dbUserData,
+        message: 'You are now logged in!'  
+      });
     });
   });
+});
+
+routes.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 // PUT /api/users/1
@@ -126,6 +153,5 @@ routes.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
 
 module.exports = routes;
